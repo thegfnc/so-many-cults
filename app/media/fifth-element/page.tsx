b5ef48254f069a25d9fce40e1519d3f3efb15cde
Jason Desiderio <jason@thegoodfornothings.club>
2025-07-09 16:52:17 -0500

    Try using different height

--- app/media/fifth-element/page.tsx
+++ app/media/fifth-element/page.tsx
@@ -5,7 +5,7 @@ import FifthElementVideos from "./FifthElementVideos";
 
 export default function FifthElement() {
   return (
-    <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[100dvh] p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center font-mono text-center">
         <FifthElementVideos />
         <FifthElementButton />
